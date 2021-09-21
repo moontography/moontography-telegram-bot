6149c449690961d19282cc65739112e59b9dc2fe
Lance Whatley <whatl3y@gmail.com>
2021-09-21 16:34:06 -0400

    Fix ETH dextools link

--- src/commands/chart.ts
+++ src/commands/chart.ts
@@ -10,7 +10,7 @@ export default function Chart(bot: Bot) {
 
       const charts = `
 *MTGY on BSC*: [https://www\\.dextools\\.io/app/pancakeswap/pair\\-explorer/0xaabafd64feb2ec235b209a95d4dc9b08e225379c](https://www.dextools.io/app/pancakeswap/pair-explorer/0xaabafd64feb2ec235b209a95d4dc9b08e225379c)
-*MTGY on ETH*: [https://www\\.dextools\\.io/app/uniswap/pair\\-explorer/0x23273b9cbcdca2210135a1b00f93b48c045fa757](https://www.dextools.io/app/uniswap/pair-explorer/0x23273b9cbcdca2210135a1b00f93b48c045fa757)
+*MTGY on ETH*: [https://www\\.dextools\\.io/app/ether/pair\\-explorer/0xc4b5b6f221d230be2f45baf5429b64803e0ab13d](https://www.dextools.io/app/ether/pair-explorer/0xc4b5b6f221d230be2f45baf5429b64803e0ab13d)
 *MTGY on KCC*: [https://kcc\\.poocoin\\.app/tokens/0xee00ef5a7ec4fdbd04eee39d60fe11c0ded65e73](https://kcc.poocoin.app/tokens/0xee00ef5a7ec4fdbd04eee39d60fe11c0ded65e73)
   `;
 
